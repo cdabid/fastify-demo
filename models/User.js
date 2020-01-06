3594127398f7f98723b8f88a156371d82c5f1717
Abid Rashid <lone.abidrashid@gmail.com>
2020-01-06 23:13:09 +0400

    check

--- models/User.js
+++ models/User.js
@@ -1,37 +1,41 @@
-const fp = require('fastify-plugin');
-const Sequelize = require('sequelize');
+const fp = require("fastify-plugin");
+const Sequelize = require("sequelize");
 
 const Model = Sequelize.Model;
 
-class User extends Model {
-}
+class User extends Model {}
 
 async function user(fastify, __) {
+  const sequelize = fastify.db;
+  User.init(
+    {
+      id: {
+        type: Sequelize.INTEGER,
+        primaryKey: true,
+        autoIncrement: true
+      },
+      name: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      email: {
+        type: Sequelize.STRING,
+        allowNull: false
+      }
+    },
+    {
+      sequelize,
+      modelName: "user"
+    }
+  );
 
-    const sequelize = fastify.db;
-
-    User.init({
-        id: {
-            type: Sequelize.INTEGER,
-            primaryKey: true,
-            autoIncrement: true
-        },
-        name: {
-            type: Sequelize.STRING,
-            allowNull: false
-        },
-        email: {
-            type: Sequelize.STRING,
-            allowNull: false
-        }
-    }, {
-        sequelize,
-        modelName: 'user'
-    });
-
-    User.sync().then(() => {
-        fastify.log.info("users table created");
-    }).catch(err => fastify.log.error("failed to creat users tables with error: " + err));
+  User.sync()
+    .then(() => {
+      fastify.log.info("users table created");
+    })
+    .catch(err =>
+      fastify.log.error("failed to creat users tables with error: " + err)
+    );
 }
 
 module.exports = fp(user);
